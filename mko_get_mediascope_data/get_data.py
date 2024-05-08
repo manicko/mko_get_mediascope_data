497fe9b8dea9e8a6d413e9ac3e3d3b90169d65ad
manicko <manicko@yandex.ru>
2024-05-08 20:21:11 +0300

    + fixed extension bug

--- mko_get_mediascope_data/get_data.py
+++ mko_get_mediascope_data/get_data.py
@@ -55,7 +55,7 @@ def get_data(
 
 
 if __name__ == '__main__':
-    REPORT_SETTINGS = 'nat_tv_brands_last.yaml'
+    REPORT_SETTINGS = 'sovcombank/nat_tv_tvreport.yaml'
     root_dir = Path(__file__).absolute().parent  # root_dir = Path().absolute()
     rep_settings_file = Path.joinpath(root_dir, 'settings/reports/', REPORT_SETTINGS)
     out_path = Path.joinpath(root_dir.parent, "data/output")
